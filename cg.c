a1232dc389f2c999613256fefc6a9d268ccb97f6
ytsteven <1647605516@qq.com>
2023-05-16 22:46:51 +0800

    fix

--- cg.c
+++ cg.c
@@ -138,7 +138,7 @@ cg_write(cg_controller controller, const char *attr, const char *fmt, ...)
   char path[256];
   cg_makepath(path, sizeof(path), controller, attr);
 
-  int fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+  int fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
   if (fd < 0)
     {
       if (maybe)
